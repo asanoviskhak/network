df656868abc6b0168c518072d6364793e8825b58
Iskhak Asanov <asanov.ishak1999@gmail.com>
2020-06-24 21:52:42 +0900

    + UI improvements + Code Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:network/weather_forecast/weather_forecast.dart';
 void main() => runApp(new MaterialApp(
-  home: WeatherForecast(),
+  home: AnnotatedRegion<SystemUiOverlayStyle>(
+    value: SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+      child: WeatherForecast()),
   debugShowCheckedModeBanner: false,
   debugShowMaterialGrid: false,
 ));
